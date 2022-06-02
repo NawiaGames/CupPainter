935fb8b6ba2a1436d65b286bce45846338a3c4bb
Anton Pashkevich <natoy7000@gmail.com>
2022-06-02 16:34:15 +0200

    Painting a brush

--- Assets/Scripts/MoveToMouse.cs
+++ Assets/Scripts/MoveToMouse.cs
@@ -1,20 +1,28 @@
 using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class MoveToMouse : MonoBehaviour
 {
     [SerializeField] private float _speed = 12.0f;
     [SerializeField] private float _distanceFromCamera = 2.0f;
     [SerializeField] private Camera _camera;
- /*   [Range(0f,1f)]
-    [SerializeField] private float _offsetMiddleScreenX = 0.2f; */
-    private Vector3 _middleScreen;
-    private void Start()
-    {
-        _middleScreen = GetMiddleScreenToWorld();
-    }
+    [SerializeField] private float _limitPositionX = 1f; 
 
-    void Update()
+
+     [SerializeField] private Color _color;
+
+     private void Start()
+     {
+         UpdateColor();
+     }
+
+     public void UpdateColor()
+     {
+         _color = Random.ColorHSV(0, 1, 0.75f, 0.75f, 0.75f, 0.75f);
+     }
+
+     void Update()
     {
         FollowerMouse();
     }
@@ -28,18 +36,18 @@ public class MoveToMouse : MonoBehaviour
         var mouseScreenToWorld = _camera.ScreenToWorldPoint(mousePosition);
         var position = Vector3.Lerp(transform.position, mouseScreenToWorld, 1.0f - Mathf.Exp(-_speed * Time.deltaTime));
         
-        if (position.x < _middleScreen.x)
+        if (position.x < _limitPositionX)
         {
-            position = new Vector3(_middleScreen.x, position.y, position.z); 
+            position = new Vector3(_limitPositionX, position.y, position.z); 
         }
         
         transform.position = position;
     }
 
-    private Vector3 GetMiddleScreenToWorld()
+/*    private Vector3 GetMiddleScreenToWorld()
     {
-        var middle = new Vector3(_camera.pixelWidth / 2, _camera.pixelHeight / 2, 0); 
+        var middle = new Vector3(_camera.pixelWidth + _offsetMiddleScreenX / 2, _camera.pixelHeight / 2, 0); 
         middle = _camera.ScreenToWorldPoint(middle);
         return middle; 
-    }
+    }*/
 }
