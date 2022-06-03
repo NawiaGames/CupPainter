2e5c64c200c9b05973f7a2b2b77e227a9f41cad2
Anton Pashkevich <natoy7000@gmail.com>
2022-06-03 11:41:59 +0200

    Ready (hardness, radius, speed)

--- Assets/Scripts/Rotation.cs
+++ Assets/Scripts/Rotation.cs
@@ -1,41 +1,15 @@
-using System;
 using UnityEngine;
 
 public class Rotation : MonoBehaviour
 {
     [SerializeField] private Vector3 _rotationSpeed;
-    [SerializeField] private int _changeSpeedRotation = 10;
-    [SerializeField] private ButtonGameUI _eventButtonGameUI;
-
-    private void Start()
-    {
-        _eventButtonGameUI.OnUpdateSpeedRotationEvent += OnUpdateSpeed;
-    }
-
-    private void OnEnable()
-    {
-        _eventButtonGameUI.OnUpdateSpeedRotationEvent -= OnUpdateSpeed;
-    }
 
     private void Update()
     {
         Rotate();
     }
 
-    private void Rotate()
-    {
-        transform.Rotate(_rotationSpeed * Time.deltaTime, Space.World);
-    }
+    private void Rotate() => transform.Rotate(_rotationSpeed * Time.deltaTime, Space.World);
 
-    private void OnUpdateSpeed(bool addSpeed)
-    {
-        if (addSpeed)
-            _rotationSpeed = new Vector3(_rotationSpeed.x, _rotationSpeed.y + _changeSpeedRotation, _rotationSpeed.z);
-        else
-        {
-            if(_rotationSpeed.y > 0 )
-                _rotationSpeed = new Vector3(_rotationSpeed.x, _rotationSpeed.y - _changeSpeedRotation, _rotationSpeed.z);
-        }
-
-    }
+    public void SetSpeed(float value) => _rotationSpeed.y = value; 
 }
--- Assets/Scripts/UI/ButtonGameUI.cs
+++ Assets/Scripts/UI/ButtonGameUI.cs
@@ -5,30 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class ButtonGameUI : MonoBehaviour
 {
-    [SerializeField] private P3dPaintSphere _paintSphere; 
-    private bool _isAddSpeed; 
+    [SerializeField] private P3dPaintSphere _paintSphere;
+    [SerializeField] private Rotation _rotationObject; 
 
-    public event UnityAction<bool> OnUpdateSpeedRotationEvent;
+    public void OnOverloadScene() => SceneManager.LoadScene(SceneManager.GetActiveScene().name); 
+    
+    public void OnChangeOpacity(float value) => _paintSphere.Opacity = value;
+    
+    public void OnChangeHardness(float value) => _paintSphere.Hardness = value;
 
-    public void OnAddSpeedRotationButton()
-    {
-        _isAddSpeed = true; 
-        OnUpdateSpeedRotationEvent?.Invoke(_isAddSpeed);
-    }
+    public void OnChangeRadius(float value) => _paintSphere.Radius = value;
 
-    public void OnSubtractSpeedRotationButton()
-    {
-        _isAddSpeed = false;
-        OnUpdateSpeedRotationEvent?.Invoke(_isAddSpeed); 
-    }
-
-    public void OnOverloadScene()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name); 
-    }
-
-    public void OnChangeOpacity(float value)
-    {
-        _paintSphere.Opacity = value; 
-    }
+    public void OnChangeSpeedRotation(float value) => _rotationObject.SetSpeed(value); 
 }
