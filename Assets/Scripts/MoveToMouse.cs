631722154a103f52411622df84fccbf931d6d362
Anton Pashkevich <natoy7000@gmail.com>
2022-06-06 18:17:07 +0200

    Update input v1.2

--- Assets/Scripts/MoveToMouse.cs
+++ Assets/Scripts/MoveToMouse.cs
@@ -1,19 +1,49 @@
 using UnityEngine;
+using UnityEngine.Serialization;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 public class MoveToMouse : MonoBehaviour
 {
-    [SerializeField] private float _speed = 12.0f;
-    [SerializeField] private float _distanceFromCamera = 2.0f;
     [SerializeField] private Camera _camera;
-    [SerializeField] private float _limitPositionX = 1f;
-    [SerializeField] private Transform _raycastTransform;
+    [Header("Settings")] [SerializeField] private float _speed = 12.0f;
+    [SerializeField] private float _distanceFromCamera = 2.0f;
+    [SerializeField] private float _offestPositionObjectZ = 0.1f;
+    [SerializeField] private float _positionBruchZ = -1f;
+    [Header("Limit position")] [SerializeField]
+    private float _limitPositionX = 1f;
+    [SerializeField] private Vector2 _limitPositionY;
+    [Header("Raycast")] [SerializeField] private Transform _raycastTransform;
     [SerializeField] private float _maxDistanceRaycast = 0.5f;
-    [SerializeField] private float _offestPositionObjectZ = 0.1f; 
+    [FormerlySerializedAs("_bruchElements")] [SerializeField]
+    private Transform _bruchTransform;
+
     private Vector3 _positionBorder = Vector3.zero;
+
+    private Vector3 offestMouse = Vector3.zero;
+ 
 
     private void Update()
     {
         FollowerMouse();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            offestMouse = _bruchTransform.position - transform.position;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            var position = transform.position + offestMouse;
+            position = LimitPosition(position);
+            _bruchTransform.position = position;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            _bruchTransform.position =
+                new Vector3(_bruchTransform.position.x, _bruchTransform.position.y, _positionBruchZ);
+        }
     }
 
     private void FollowerMouse()
@@ -23,14 +53,25 @@ public class MoveToMouse : MonoBehaviour
         var mouseScreenToWorld = _camera.ScreenToWorldPoint(mousePosition);
         var position = Vector3.Lerp(transform.position, mouseScreenToWorld, 1.0f - Mathf.Exp(-_speed * Time.deltaTime));
 
+        transform.position = position;
+    }
+
+    private Vector3 LimitPosition(Vector3 position)
+    {
         if (position.x < _limitPositionX)
-            position = new Vector3(_limitPositionX, position.y, position.z);
+            position.x = _limitPositionX;
+
+        if (position.y < _limitPositionY.x)
+            position.y = _limitPositionY.x;
+
+        if (position.y > _limitPositionY.y)
+            position.y = _limitPositionY.y; 
 
         _positionBorder = MoveBorderPaint();
-        if(_positionBorder.z > position.z)
-            position = new Vector3(position.x, position.y, _positionBorder.z + _offestPositionObjectZ);
-
-        transform.position = position;
+        if (position.z < _positionBorder.z)
+            position.z = _positionBorder.z + _offestPositionObjectZ;
+        
+        return position;
     }
 
     private Vector3 MoveBorderPaint()
@@ -41,13 +82,12 @@ public class MoveToMouse : MonoBehaviour
         Debug.DrawRay(raycastPosition, raycastDirection * _maxDistanceRaycast, Color.green);
 
         if (borderForwardInfo.collider != true)
-            return new Vector3(0,0, -10);
+            return new Vector3(0, 0, _positionBruchZ);
         
-
         var positionBorder = borderForwardInfo.point;
         return positionBorder;
     }
-
+    
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.black;
