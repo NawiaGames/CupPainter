de595164cad8d3f9b5627505e19d7ed162cd2ea5
Anton Pashkevich <natoy7000@gmail.com>
2022-06-10 11:51:04 +0200

    Update Code MoveToMouse
    - create new Scripts
    - fix bug (BlendShapesBruch)

---
+++ Assets/Scripts/Bruch/ControllerMouse.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(MoveToMouse))]
+public class ControllerMouse : MonoBehaviour
+{
+    private MoveToMouse _moveToMouse;
+
+    private void Start()
+    {
+        _moveToMouse = GetComponent<MoveToMouse>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            _moveToMouse.СalculateOffestPositionMouseToBrush();
+
+        if (Input.GetMouseButton(0))
+            _moveToMouse.CalculateMovePositionBrush();
+
+        if (Input.GetMouseButtonUp(0))
+            _moveToMouse.ResetBrushButtonUp();
+
+        if (!EventSystem.current.IsPointerOverGameObject())
+            _moveToMouse.MoveBrush();
+    }
+}
--- Assets/Scripts/Bruch/MoveToMouse.cs
+++ Assets/Scripts/Bruch/MoveToMouse.cs
@@ -1,7 +1,4 @@
-using System;
-using PaintIn3D;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 public class MoveToMouse : MonoBehaviour
 {
@@ -10,34 +7,31 @@ public class MoveToMouse : MonoBehaviour
     [SerializeField] private float _distanceFromCamera = 2.0f;
     [SerializeField] private float _offestPositionObjectZ = 0.1f;
     [SerializeField] private float _offestPositionBorder = 0.1f;
-
     [Header("Limit position")] [SerializeField]
     private float _limitPositionX = 1f;
-
     [Header("Raycast")] [SerializeField] private Transform _raycastTransform;
     [SerializeField] private float _maxDistanceRaycast = 0.5f;
-
     [Header("Brush")] [SerializeField] private Transform _brushTransform;
     [SerializeField] private SkinnedMeshRenderer _brushSkinnedMeshRenderer;
     [SerializeField] private float _positionBruchZ = -1f;
 
-    private LimitPositionBrush _limitPositionBrush; 
+    private LimitPositionBrush _limitPositionBrush;
     private Vector3 _positionBorder = Vector3.zero;
     private Vector3 _offestMouse = Vector3.zero;
     private Vector3 _rightEdge;
     private Vector3 _upEdge;
     private Vector3 _positionTargetMouse;
     private float _blendShapesBrush;
-    private float _currentBlendShapesBruch; 
+    private float _currentBlendShapesBruch;
     private bool _isEndBorder;
 
     private void Start()
     {
         _brushTransform.position =
             new Vector3(_brushTransform.position.x, _brushTransform.position.y, _positionBruchZ);
-        
+
         SetLimitPosition();
-        
+
         _positionTargetMouse = _brushTransform.position;
     }
 
@@ -53,45 +47,36 @@ public class MoveToMouse : MonoBehaviour
     private void Update()
     {
         FollowerMouse();
-        ControllerMouse();
         UpdateBlendShapesBrush();
     }
 
-    private void ControllerMouse()
+    public void MoveBrush() => _brushTransform.position =
+        Vector3.Lerp(_brushTransform.position, _positionTargetMouse, _speed * Time.deltaTime);
+
+    public void СalculateOffestPositionMouseToBrush() => _offestMouse = _brushTransform.position - transform.position;
+
+    public void CalculateMovePositionBrush()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            _offestMouse = _brushTransform.position - transform.position;
-        }
+        _positionTargetMouse = transform.position + _offestMouse;
 
-        if (Input.GetMouseButton(0))
-        {
-            _positionTargetMouse = transform.position + _offestMouse;
+        _positionTargetMouse = _limitPositionBrush.Limit(_positionTargetMouse);
 
-            _positionTargetMouse = _limitPositionBrush.Limit(_positionTargetMouse);
+        if (TryMoveBorderPaintZ(out var result, _positionTargetMouse))
+            _positionTargetMouse = result;
 
-            if (TryMoveBorderPaintZ(out var result, _positionTargetMouse))
-            {
-                _positionTargetMouse = result;
-            }
-        }
+    }
 
-        if (Input.GetMouseButtonUp(0))
-        {
-            _positionTargetMouse.z = _positionBruchZ;
-            _positionTargetMouse.x = _rightEdge.x - _offestPositionBorder;
-            _blendShapesBrush = 0f;
-        }
-
-        if (!EventSystem.current.IsPointerOverGameObject())
-            _brushTransform.position =
-                Vector3.Lerp(_brushTransform.position, _positionTargetMouse, _speed * Time.deltaTime);
+    public void ResetBrushButtonUp()
+    {
+        _positionTargetMouse.z = _positionBruchZ;
+        _positionTargetMouse.x = _rightEdge.x - _offestPositionBorder;
+        _blendShapesBrush = 0f;
     }
 
     private void UpdateBlendShapesBrush()
     {
-        _currentBlendShapesBruch = Mathf.Lerp(_currentBlendShapesBruch, _blendShapesBrush, _speed/2 * Time.deltaTime);
-        _brushSkinnedMeshRenderer.SetBlendShapeWeight(0,_currentBlendShapesBruch);
+        _currentBlendShapesBruch = Mathf.Lerp(_currentBlendShapesBruch, _blendShapesBrush, _speed / 2 * Time.deltaTime);
+        _brushSkinnedMeshRenderer.SetBlendShapeWeight(0, _currentBlendShapesBruch);
     }
 
     private void FollowerMouse()
@@ -109,8 +94,9 @@ public class MoveToMouse : MonoBehaviour
         Physics.Raycast(raycastPosition, raycastDirection, out var borderForwardInfo, _maxDistanceRaycast);
         Debug.DrawRay(raycastPosition, raycastDirection * _maxDistanceRaycast, Color.green);
         result = Vector3.zero;
+        _blendShapesBrush = 0f;
         if (borderForwardInfo.collider != true) return false;
-        
+
         _blendShapesBrush = 100f;
         var positionBorder = borderForwardInfo.point;
         position.z = positionBorder.z + _offestPositionObjectZ;
