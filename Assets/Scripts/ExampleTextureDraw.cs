60738f9a6fbd62e05ac89d3270a0663da2093973
Anton Pashkevich <natoy7000@gmail.com>
2022-06-21 16:14:08 +0200

    switching animation (ExampleTextureDraw)

--- Assets/Scripts/ExampleTextureDraw.cs
+++ Assets/Scripts/ExampleTextureDraw.cs
@@ -1,30 +1,12 @@
-using System;
 using UnityEngine;
 
 public class ExampleTextureDraw : MonoBehaviour
 {
     [SerializeField] private MeshRenderer _meshRenderer;
-    [SerializeField] private float _leftX = -3.8f;
-    [SerializeField] private float _rightX = -0.8f;
-    [SerializeField] private float _speedMove = 7f;
-
-    private Vector3 _direction;
-    private Transform _myTransform;
-    private bool _changeDirection; 
-
-    private void Start()
-    {
-        _myTransform = gameObject.transform;
-        _direction = _myTransform.position;
-        _direction.x = _leftX; 
-    }
-
-    private void Update()
-    {
-        // if (_direction != _myTransform.position)
-        //     _myTransform.position = Vector3.MoveTowards(_myTransform.position, _direction, _speedMove * Time.deltaTime); 
-    }
-
+    [SerializeField] private Animator _animator; 
+    
+    private bool _changeDirection;
+    
     public void SetTexture(Texture2D texture2D) => _meshRenderer.material.mainTexture = texture2D;
 
     public void SetHeight(float height)
@@ -36,6 +18,7 @@ public class ExampleTextureDraw : MonoBehaviour
     public void UpdateDirection()
     {
         _changeDirection = !_changeDirection;
-        _direction.x = _changeDirection ? _rightX : _leftX;
+
+        _animator.SetTrigger(_changeDirection ? "activate" : "deactivate");
     }
 }
