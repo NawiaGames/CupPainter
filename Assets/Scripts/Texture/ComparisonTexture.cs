1b60d99fea1a062e3542c9384b36c4852b3e48cb
Anton Pashkevich <natoy7000@gmail.com>
2022-06-23 16:45:31 +0200

    Updated code for FPS

--- Assets/Scripts/Texture/ComparisonTexture.cs
+++ Assets/Scripts/Texture/ComparisonTexture.cs
@@ -12,8 +12,6 @@ public class ComparisonTexture : MonoBehaviour
     [SerializeField] private Texture2D _texture2DCurrentDraw;
     [SerializeField] private float _accurateBetweenTextures = 0.1f;
 
-    [SerializeField] private Material _blitCheckerMaterial; 
-    
     private int _comparisonPixel;
     private float _pixelPercent;
     private int _sizePixel;
@@ -28,7 +26,6 @@ public class ComparisonTexture : MonoBehaviour
     {
         StopAllCoroutines();
         StartCoroutine("ComparisonPixelDrawing");
-    //    ComparisonPixelDrawing3();
     }
 
     public IEnumerator ComparisonPixelDrawing()
@@ -62,53 +59,6 @@ public class ComparisonTexture : MonoBehaviour
         
         _comparisonText.gameObject.SetActive(true);
         _comparisonText.text = "Successed: " + result.ToString("F1") + "%";
-    }
-
-    public void ComparisonPixelDrawing3()
-    {
-        _comparisonPixel = 0;
-        _texture2DDraw = createLevel.Texture2DModelsSample[SelectedPaintObjects.CurrentPaintObjectIndex];
-        _texture2DCurrentDraw = PaintTexture.toTexture2D(createLevel
-            .PaintObjects[SelectedPaintObjects.CurrentPaintObjectIndex].RenderTexturePaint);
-        
-        var colorOne = _texture2DCurrentDraw.GetPixels32();
-        var colorTwo = _texture2DDraw.GetPixels32();
-        
-        for (var i = 0; i < colorOne.Length; i++)
-        {
-            if ((colorOne[i].r <= colorTwo[i].r + _accurateBetweenTextures &&
-                 colorOne[i].r >= colorTwo[i].r - _accurateBetweenTextures)
-                && ((colorOne[i].g <= colorTwo[i].g + _accurateBetweenTextures &&
-                     colorOne[i].g >= colorTwo[i].g - _accurateBetweenTextures))
-                && (colorOne[i].b <= colorTwo[i].b + _accurateBetweenTextures &&
-                    colorOne[i].b >= colorTwo[i].b - _accurateBetweenTextures)
-                && (colorOne[i].a <= colorTwo[i].a + _accurateBetweenTextures &&
-                    colorOne[i].a >= colorTwo[i].a - _accurateBetweenTextures))
-                    _comparisonPixel++;
-        }
-
-        var result = _comparisonPixel * _pixelPercent;
-        Debug.Log("Successed: " + result.ToString("F1") + "%");
-    }
-    
-    public void ComparisonPixelDrawing2()
-    {
-        _texture2DDraw = createLevel.Texture2DModelsSample[SelectedPaintObjects.CurrentPaintObjectIndex];
-        _texture2DCurrentDraw = PaintTexture.toTexture2D(createLevel
-            .PaintObjects[SelectedPaintObjects.CurrentPaintObjectIndex].RenderTexturePaint);
-
-        _blitCheckerMaterial.SetTexture("_SourceTex", _texture2DDraw);
-        var newTex = new Texture2D(1, 1);
-        var myRT = new RenderTexture(1, 1, 24);
-        Graphics.Blit(_texture2DCurrentDraw, myRT, _blitCheckerMaterial);
-        RenderTexture.active = myRT; 
-        newTex.ReadPixels(new Rect(0,0,1,1), 0, 0);
-        myRT.Release();
-
-        var px = newTex.GetPixel(1, 1);
-        Debug.Log(px);
-        Debug.Log("match " + (100-(px.r + px.g + px.b)/3*100) + " %");
-        
     }
 
     public void OnEnableText() => _comparisonText.gameObject.SetActive(false);
