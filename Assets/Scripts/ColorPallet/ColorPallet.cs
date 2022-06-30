d1025a656c3b4e99471ec4b735f1f11e74d90bfd
Anton Pashkevich <natoy7000@gmail.com>
2022-06-30 15:10:40 +0200

    Smoothness :(

--- Assets/Scripts/ColorPallet/ColorPallet.cs
+++ Assets/Scripts/ColorPallet/ColorPallet.cs
@@ -6,15 +6,23 @@ public class ColorPallet : MonoBehaviour
 {
     private Renderer _renderer;
     private int _index;
-    
+
     private void Awake()
     {
         _renderer = GetComponent<Renderer>(); 
     }
 
     public void SetColor(Color color) => _renderer.material.color = color;
-
+    
     public Color GetColor() => _renderer.material.color;
+
+    public void SetSmoothness(float smoothness, Color colorSmoothness)
+    {
+        const string nameSmoothness = "_Smoothness";
+        const string nameSpecColor = "_SpecColor"; 
+        _renderer.material.SetFloat(nameSmoothness, smoothness);
+        _renderer.material.SetColor(nameSpecColor, colorSmoothness);
+    }
 
     public void SetIndex(int value) => _index = value;
     public int GetIndex() => _index; 
