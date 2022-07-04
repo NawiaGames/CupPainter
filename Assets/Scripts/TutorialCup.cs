2f677ed79a41958e298baa75ce8260f0a1906b9e
Anton Pashkevich <natoy7000@gmail.com>
2022-07-04 16:01:45 +0200

    Tutorial

--- Assets/Scripts/TutorialCup.cs
+++ Assets/Scripts/TutorialCup.cs
@@ -6,18 +6,48 @@ using UnityEngine;
 public class TutorialCup : MonoBehaviour
 {
     [SerializeField] private InputOverlayTutorial _inputOverlayTutorial;
+    [SerializeField] private Transform _compareTransform; 
+    [SerializeField] private float _timeNextStep = 3f;
+
+    private bool _nextStep = true;
+
+    private void Start()
+    {
+        Debug.Log(_compareTransform.position);
+        _inputOverlayTutorial.Activate(_compareTransform.position);
+    }
 
     public void StartTutorial()
     {
+     /*   StopAllCoroutines();
+        
         if (SelectedPaintObjects.CurrentPaintObjectIndex == 0)
         {
             StartCoroutine(StartTutorialLevel1Coroutine());
-        }
+        }*/
     }
 
     private IEnumerator StartTutorialLevel1Coroutine()
     {
-        _inputOverlayTutorial.Activate(Vector3.down * 30f);
+        var position = new Vector3[]{ Vector3.down * 30f, _compareTransform.position};
+        _nextStep = true; 
+        for (var i = 0; i < position.Length; )
+        {
+            if (_nextStep)
+            {
+                _nextStep = false;
+                StartCoroutine(WaitClick(position[i]));
+                i++;
+                yield return new WaitForSeconds(_timeNextStep);
+            }
+            yield return null; 
+        }
+        
+    }
+
+    private IEnumerator WaitClick(Vector3 position)
+    {
+        _inputOverlayTutorial.Activate(position);
 
         while (true)
         {
@@ -27,5 +57,7 @@ public class TutorialCup : MonoBehaviour
         }
         
         _inputOverlayTutorial.Deactivate();
+        _nextStep = true; 
     }
+    
 }
