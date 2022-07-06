44a904107782aff0f1d2a93678e9ab986426d2cf
ngmroz <71435149+ngmroz@users.noreply.github.com>
2022-07-06 11:47:30 +0200

    -bb, fb

--- Assets/Analytics/AnalyticsEvents.cs
+++ Assets/Analytics/AnalyticsEvents.cs
@@ -3,20 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using LionStudios.Suite.Analytics;
-#if HAS_LION_GAME_ANALYTICS_SDK
-using GameAnalyticsSDK;
-#endif
-#if HAS_BYTEBREW_SDK
+//#if HAS_BYTEBREW_SDK
 using ByteBrewSDK;
-#endif
+//#endif
 
 public class AnalyticsEvents : MonoBehaviour
 {
   void Awake()
   {
-#if HAS_BYTEBREW_SDK
+  #if HAS_BYTEBREW_SDK
     ByteBrewInit();
-#endif
+  #endif
 
     EventManager.OnLevelStart += on_level_started;
     EventManager.OnLevelSuccessed += on_level_successed;
@@ -34,7 +31,7 @@ public class AnalyticsEvents : MonoBehaviour
 #if HAS_BYTEBREW_SDK
   void ByteBrewInit()
   {
-#if UNITY_IOS
+  #if UNITY_IOS
     ByteBrew.requestForAppTrackingTransparency((status) =>
     {
       //Case 0: ATTrackingManagerAuthorizationStatusAuthorized
@@ -44,9 +41,9 @@ public class AnalyticsEvents : MonoBehaviour
       Debug.Log("ByteBrew Got a status of: " + status);
       ByteBrew.InitializeByteBrew();
     });
-#else
+  #else
     ByteBrew.InitializeByteBrew();
-#endif
+  #endif
   }
 #endif
 
@@ -54,17 +51,14 @@ public class AnalyticsEvents : MonoBehaviour
   {
     LionAnalytics.LevelStart(lvl + 1, 0);
   #if HAS_BYTEBREW_SDK
-    ByteBrew.NewProgressionEvent(ByteBrewProgressionTypes.Started, "Level", "", GameState.Progress.Level + 1);
+    ByteBrew.NewProgressionEvent(ByteBrewProgressionTypes.Started, "Level", "", lvl + 1);
   #endif
-  #if HAS_LION_GAME_ANALYTICS_SDK
-    GameAnalytics.NewProgressionEvent(GAProgressionStatus.Start, string.Format("Level_{0:D3}", lvl + 1));
-  #endif    
   }
   void on_level_restarted(int lvl)
   {
-  #if HAS_LION_GAME_ANALYTICS_SDK
-    LionAnalytics.LevelRestart(lvl+ 1, 0);
-  #endif 
+    #if HAS_BYTEBREW_SDK
+      //ByteBrew.NewProgressionEvent(ByteBrewProgressionTypes.LevelRestart, "Level", "", lvl + 1);
+    #endif  
   }
   void on_level_successed(int lvl)
   {
@@ -82,9 +76,6 @@ public class AnalyticsEvents : MonoBehaviour
     #if HAS_BYTEBREW_SDK
       ByteBrew.NewProgressionEvent(ByteBrewProgressionTypes.Completed, "Level", "", lvl + 1);
     #endif
-    #if HAS_LION_GAME_ANALYTICS_SDK
-      //GameAnalytics.NewProgressionEvent(GAProgressionStatus.Complete, string.Format("Level_{0:D3}", GameState.Progress.Level + 1));
-    #endif  
     }
     else
     {
@@ -92,9 +83,6 @@ public class AnalyticsEvents : MonoBehaviour
     #if HAS_BYTEBREW_SDK
       ByteBrew.NewProgressionEvent(ByteBrewProgressionTypes.Failed, "Level", "", lvl + 1);
     #endif
-    #if HAS_LION_GAME_ANALYTICS_SDK
-      //GameAnalytics.NewProgressionEvent(GAProgressionStatus.Fail, string.Format("Level_{0:D3}", GameState.Progress.Level + 1));
-    #endif  
     }
   }  
   void on_invoke_rewarded_ad(string placement)
